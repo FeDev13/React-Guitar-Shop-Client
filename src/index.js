05d3dd543c98ef1ede02834034f0d691a85a79a2
Federico Robledo <federicorobledo85@gmail.com>
2023-03-03 10:15:56 -0300

    deploy

--- src/index.js
+++ src/index.js
@@ -6,9 +6,9 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { Context } from "./Contexts/Context";
-/* import { disableReactDevTools } from "@fvilers/disable-react-devtools"; */
+import { disableReactDevTools } from "@fvilers/disable-react-devtools";
 
-/* if (process.env.NODE_ENV === "production") disableReactDevTools(); */
+if (process.env.NODE_ENV === "production") disableReactDevTools();
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
